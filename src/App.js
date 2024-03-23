c161a8c9aa89cf94b65053567ef35a1432fad800
danyal <danyal.titanka24@gmail.com>
2024-03-23 16:39:45 +0330

    Create Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Header } from './Header/Header';
+import { Home } from './Home/Home';
 
-function App() {
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header-background">
+      <div className='container' id='container'>
+        <Header />
+        <Home />
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Header/Header.js
@@ -0,0 +1,44 @@
+import React, { useState , useEffect} from 'react';
+import "./Header.css";
+import { IoRocketOutline } from "react-icons/io5";
+
+export const Header = () => {
+
+  const [scroll,setScroll] = useState(false);
+
+  useEffect(()=>{
+
+    const $ = window ;
+
+    const scrollCounter = $.addEventListener("scroll",(e)=>{
+      const roundedScrollCount = Math.round($.scrollY);
+      if(roundedScrollCount >= 1) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+
+    });
+
+  },[])
+
+  return (
+    <nav className={scroll ? "navbar navbar-expand-md py-3 fixed-top bg-white" : "navbar navbar-expand-md py-2s"}>
+      <div className="container" style={{width:scroll ? "75%" : ""}}>
+        <a className='navbar-brand'><IoRocketOutline />Danyal</a>
+        <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#nav-header">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="nav-header">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item px-2"><a href="#home" className="nav-link" style={{color:scroll?"#6C55A3" : ""}}>خانه</a></li>
+            <li className="nav-item px-2"><a href="#about" className="nav-link" style={{color:scroll?"#6C55A3" : ""}}>درباره</a></li>
+            <li className="nav-item px-2"><a href="" className="nav-link" style={{color:scroll?"#6C55A3" : ""}}>خدمات</a></li>
+            <li className="nav-item px-2"><a href="" className="nav-link" style={{color:scroll?"#6C55A3" : ""}}>رضایت نامه ها</a></li>
+            <li className="nav-item px-2"><a href="#contact" className="nav-link" style={{color:scroll?"#6C55A3" : ""}}>تماس</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
