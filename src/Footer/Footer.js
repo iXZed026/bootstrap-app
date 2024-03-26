9802f03b3b14b6c83e73028d761d45accad7d1ca
danyal <danyal.titanka24@gmail.com>
2024-03-26 22:14:46 +0330

    Create Footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <div id="footer" className="p-4 text-center text-white fs-5">
+        2024 © Danyal. All Rights Reserved.
+    </div>
+  )
+}
